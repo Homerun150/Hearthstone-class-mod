8ea2164bfbc53b8ca61df9d485f0a1d0311cf55b
Homerun150 <cameronabrams@aol.com>
2017-04-20 08:42:35 -0400

    Rename a file

---
+++ HearthstoneModGame/HearthstoneModGame/HearthstoneModGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HearthstoneModGame")]
+[assembly: AssemblyProduct("HearthstoneModGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Ingham Intermediate School District")]
+[assembly: AssemblyCopyright("Copyright © Ingham Intermediate School District 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("0fdf1e34-c371-4e7b-9b34-1b731b76100e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
