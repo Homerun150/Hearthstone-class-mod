1df38b45302d71c46835661a7e6331e980c0c767
Homerun150 <cameronabrams@aol.com>
2017-04-25 10:22:33 -0400

    update button

--- HearthstoneModGame/HearthstoneModGame/HearthstoneModGame/Buttoncollision.cs
+++ HearthstoneModGame/HearthstoneModGame/HearthstoneModGame/Buttoncollision.cs
@@ -15,5 +15,16 @@ namespace HearthstoneModGame
 {
     class Buttoncollision
     {
+        private Texture2D buttonImage;
+        private Rectangle buttonCollision;
+        private Vector2 buttonPosition;
+        private Vector2 mousePosition;
+
+        public ButtonCollision(ContentManager Content, Vector2 ButtonPosition, string ButtonImage)
+        {
+            buttonPosition = ButtonPosition;
+            buttonImage = Content.Load<Texture2D>(ButtonImage);
+            buttonCollision = new Rectangle((int)ButtonPosition.X, (int)ButtonPosition.Y, (int) Button
+        }
     }
 }
